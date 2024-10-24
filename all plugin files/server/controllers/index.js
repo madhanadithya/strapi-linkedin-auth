9ac990538b2bb8b081c6843155600bc56ea2c491
MADHAN ADITHYA KR <116862390+madhanadithya@users.noreply.github.com>
2024-10-24 15:10:34 +0530

    Delete all plugin files directory

--- all plugin files/server/controllers/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const myController = require('./my-controller');
-const linkedinAuthController = require('./linkedin-auth');
-
-module.exports = {
-  myController,
-  linkedinAuthController
-};
